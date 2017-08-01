3e7f2e22b6646e9717824ae61b67634b9ad032cf
Moses Esan <mosesesan@hotmail.com>
2017-08-01 16:21:24 -0400

    Updated routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-
-Route::get('user/verify/{verification_code}', 'AuthController@verifyUser');
